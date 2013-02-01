64e34f2d038ba8f4be79adc5e2d4ff93d617feaf
Erwan Loisant <eloisant@gmail.com>
2013-02-01 16:36:29 +0100

    Remove deps on MySQL

--- app/models/AppDB.scala
+++ app/models/AppDB.scala
@@ -2,13 +2,15 @@ package models
 
 import play.api.Application
 
-import scala.slick.driver.MySQLDriver.simple._
+import scala.slick.session.Database
 
 object AppDB {
 
   def db(implicit app: Application) = Database.forURL(
     app.configuration.getString("db.default.url").getOrElse(sys.error("Missing key: db.default.url")),
     app.configuration.getString("db.default.user").getOrElse(""),
-    driver = "scala.slick.driver.MySQLDriver")
+    driver = app.configuration.getString("slick.driver").getOrElse(sys.error("Slick: missing configuration key slick.driver"))
+  )
 
 }
+
